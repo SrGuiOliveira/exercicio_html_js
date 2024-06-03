866b0aa5aa31d8ddbbdab48fc88b7888d68ec9a4
Guilherme Oliveira <gui.live012@gmail.com>
2024-06-03 14:53:22 -0300

    Exercicio pronto para entrega!

--- OneDrive/Documentos/EBAC/FrontEnd/exercicios/exercicio_html_js/script.js
+++ OneDrive/Documentos/EBAC/FrontEnd/exercicios/exercicio_html_js/script.js
@@ -1,19 +1,26 @@
 function verificar() {
     var campoA = Number(document.getElementById("campoA").value)
     var campoB = Number(document.getElementById("campoB").value)
+    var mensagem = document.getElementById("mensagem")
 
     if (campoB > campoA) {
 
         alert(`Validado com sucesso! O campo B de valor ${campoB} é maior que o campo A de valor ${campoA}`)
+        mensagem.innerHTML = `Entregue com sucesso!`
+        mensagem.style.color = "#218f30"
 
 
     } else if (campoA > campoB) {
 
         alert("Erro! O valor do campo B deve ser maior que o campo A!")
+        mensagem.innerHTML = `Erro! Tente novamente...`
+        mensagem.style.color = "#ec1224"
 
     } else {
 
-        alert("Valores inválidos! Tente novamente (apenas números!)")
+        alert("Valores inválidos! Tente novamente")
+        mensagem.innerHTML = `lembre-se de preencher ambos os campos...`
+        mensagem.style.color = "#d1c40c"
 
     }
 
